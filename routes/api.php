634ff40d022bba73f1d7d667fb2be6c1335b35e1
giang03yn@gmail.com <giang03yn@gmail.com>
2023-09-30 21:37:06 +0700

    init project

---
+++ routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::prefix('v1')->group(function () {
+
+    Route::get('/', function () {
+        $datas = [
+            ['id' => 1, 'name' => 'Giang', 'age' => 20],
+            ['id' => 2, 'name' => 'Giang', 'age' => 20],
+            ['id' => 3, 'name' => 'Giang', 'age' => 20]
+        ];
+
+        return response()->json([
+            'data' => $datas
+        ]);
+    });
+});
