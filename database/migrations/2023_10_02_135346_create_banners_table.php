027faad64433f0961bb32f22c35d0be1be3c9395
thanhli3m <ngocthanhkun@gmail.com>
2023-10-02 20:57:13 +0700

    set up sample database

---
+++ database/migrations/2023_10_02_135346_create_banners_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('banners', function (Blueprint $table) {
+            $table->id();
+            $table->string('banner_name');
+            $table->string('banner_description'); // Mô tả 
+            $table->string('banner_image'); // Ảnh
+            $table->date('expired_at'); // hết hạn
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('banners');
+    }
+};
