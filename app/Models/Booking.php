1e4c9f6aa6b254e44c8834164f3a832ea651ff6e
thanhli3m <ngocthanhkun@gmail.com>
2023-10-10 14:52:44 +0700

    update fillable for models

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Booking extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'showtime_id',
+        'seat_id',
+        'product_id',
+    ];
 }
