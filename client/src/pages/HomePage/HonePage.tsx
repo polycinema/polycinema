a1f80e435ac3d857bcaf05fad22617a148967ae0
Giangchuate <105421404+Giangchuate@users.noreply.github.com>
2023-10-11 21:54:32 +0700

    feature:movie-page

---
+++ client/src/pages/HomePage/HonePage.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import BannerMovie from '../../components/banner/BannerMovie'
+import ListMovie from '../../components/listmovie/ListMovie'
+
+
+
+const HonePage = () => {
+        return (
+        <>
+        <BannerMovie/>
+        <ListMovie/>
+        </>
+
+        )
+}
+
+export default HonePage
