475ceb21d1aa5eb265f886face2f303b89b44257
Giangchuate <105421404+Giangchuate@users.noreply.github.com>
2023-10-25 00:40:12 +0700

    feature: test api

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -5,6 +5,8 @@ import MoviePage from "./pages/MoviePage/MoviePage";
 import Account from "./auth/Account";
 import NewsPage from "./pages/NewsPage/NewsPage";
 import LayoutAdmin from "./layouts/LayoutAdmin";
+import ListGenre from "./pages/admin/GenreMng/ListGenre";
+import AddGenre from "./pages/admin/GenreMng/AddGenre";
 export const router = createBrowserRouter([
         {path:'', element:<LayoutWebsite/>, children:[
                 {path:'', element:<HomePage/>},
@@ -13,6 +15,9 @@ export const router = createBrowserRouter([
                 {path:'poly-news',element:<NewsPage/>},
         ]},
         {path: 'admin', element: <LayoutAdmin/>,children:[
+                {path:'genres',element:<ListGenre/>},
+                {path:'genres/add',element:<AddGenre/>},
+                {path:'genres/:id/edit',element:<ListGenre/>},
 
         ]}
 
