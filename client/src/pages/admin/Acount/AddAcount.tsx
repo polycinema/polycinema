a579826a1a622113539fd7dd57d2f0ec2f5d6e36
Hoangldph21518 <Hoangldph21518@fpt.edu.vn>
2023-11-02 23:33:54 +0700

    feature/crud_Acount

---
+++ client/src/pages/admin/Acount/AddAcount.tsx
@@ -0,0 +1,102 @@
+import React from 'react'
+import { Button, Form, Input, message } from 'antd';
+import { useNavigate } from 'react-router';
+import { pause } from '../../../utils/pause';
+import { addAcount } from '../../../api/Acount';
+import { Select } from 'antd';
+
+
+type FieldType = {
+    name: string;
+    email: string;
+    password: string;
+};
+
+const AddAcount = () => {
+
+    const [form] = Form.useForm()
+    const [messageApi, contextHolder] = message.useMessage()
+    const navigate = useNavigate();
+
+    const onFinish = (values) => {
+        addAcount(values)
+            .then(async () => {
+                form.resetFields()
+                messageApi.open({
+                    type: "success",
+                    content: "Thêm tài khoản thành công , Chuyển trang sau 3s"
+                })
+                await pause(3000)
+                navigate("/admin/acount")
+            })
+            .catch((err) => {
+                console.log(err);
+
+            })
+
+
+    };
+
+    const onFinishFailed = (errorInfo: any) => {
+        console.log('Failed:', errorInfo);
+    };
+    return (
+        <>
+            {contextHolder}
+            <div>
+                <h1 className='text-4xl m-6'>Thêm tài khoản</h1>
+                <Form
+
+                    name="basic"
+                    labelCol={{ span: 8 }}
+                    wrapperCol={{ span: 16 }}
+                    style={{ maxWidth: 600 }}
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
+                >
+                    <Form.Item<FieldType>
+                        label="Tên người dùng"
+                        name="name"
+                        rules={[{ required: true, message: 'Please input your name!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item<FieldType>
+                        label="Email"
+                        name="email"
+                        rules={[{ required: true, message: 'Please input your email!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item<FieldType>
+                        label="Password"
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+
+
+                    <Form.Item
+                        wrapperCol={{ offset: 8, span: 16 }}
+                        label="Tác vụ :"
+
+                    >
+
+                        <Button
+                            htmlType="submit">
+                            Thêm
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
+        </>
+
+    )
+}
+
+export default AddAcount
