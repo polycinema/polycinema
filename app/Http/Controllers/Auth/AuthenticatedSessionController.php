27d22e066b74e916b195aea0f3a5a24346a2e370
giang03yn@gmail.com <giang03yn@gmail.com>
2023-11-08 21:30:39 +0700

    delete breeze package

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,22 +4,31 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class AuthenticatedSessionController extends Controller
 {
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): Response
+    public function store(LoginRequest $request): JsonResponse
     {
         $request->authenticate();
 
-        $request->session()->regenerate();
+        $user = $request->user();
 
-        return response()->noContent();
+        $user->tokens()->delete();
+
+        $token = $user->createToken('api-token');
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ], Response::HTTP_OK);
     }
 
     /**
