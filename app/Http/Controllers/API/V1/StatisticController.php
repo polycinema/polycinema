99d256d655fa2ff14f30b70819f199bec6001d3e
giang03yn@gmail.com <giang03yn@gmail.com>
2023-12-18 21:38:08 +0700

    statistic controller

---
+++ app/Http/Controllers/API/V1/StatisticController.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Booking;
+use Carbon\Carbon;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class StatisticController extends Controller
+{
+
+    /**
+     *
+     * @func getStatisticInDay
+     * get the booking in current day
+     */
+
+    public function getStatisticInDay()
+    {
+        try {
+        } catch (Exception $exception) {
+            Log::error('StatisticController@getStatisticInDay: ', [$exception->getMessage()]);
+
+            return response()->json([
+                'message' => 'Đã có lỗi nghiêm trọng xảy ra'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function getStatisticInWeek()
+    {
+        try {
+            // $today = now()->toDateString();
+            $startOfLast7Days = now()->subDays(6)->startOfDay(); //Start date is last 7 days
+            $endOfToday = now()->endOfDay();
+
+            $allDaysLast7Days = [];
+            $currentDay = clone $startOfLast7Days;
+            while ($currentDay <= $endOfToday) {
+                $allDaysLast7Days[] = $currentDay->toDateString();
+                $currentDay->addDay();
+            }
+
+            $weeklyBookings = Booking::whereBetween('created_at', [$startOfLast7Days, $endOfToday])
+                ->select(DB::raw('DATE(created_at) as date'), DB::raw('count(*) as booking_count'))
+                ->groupBy('date')
+                ->get();
+
+            // create new array with default value is 0, set all day off week is the last 7 days
+            $result = [];
+            $totalRevenue = 0;
+            foreach ($allDaysLast7Days as $day) {
+                $booking = $weeklyBookings->firstWhere('date', $day);
+
+                // total revenue a day
+                // $revenue = Booking::whereDate('created_at', $day)->sum('total_price');
+                $revenue = (int) Booking::whereDate('created_at', $day)->sum('total_price');
+
+                $result[] = [
+                    'date' => $day,
+                    'booking_count' => $booking ? $booking->booking_count : 0,
+                    'revenue' => $revenue
+                ];
+
+                $totalRevenue += $revenue;
+            }
+
+            // $weeklyBookings = $weeklyBookings->map(function ($item) {
+            //     $item->day_name = Carbon::parse($item->date)->format('l');
+            //     return $item;
+            // });
+
+            return response()->json([
+                'data' => [
+                    'weekly_bookings' => $result,
+                    'total_revenue' => $totalRevenue
+                ]
+            ], Response::HTTP_OK);
+        } catch (Exception $exception) {
+            Log::error('StatisticController@getStatisticInWeek: ', [$exception->getMessage()]);
+
+            return response()->json([
+                'message' => 'Đã có lỗi nghiêm trọng xảy ra'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function getStatisticInMonth()
+    {
+        try {
+        } catch (Exception $exception) {
+            Log::error('StatisticController@getStatisticInMonth: ', [$exception->getMessage()]);
+
+            return response()->json([
+                'message' => 'Đã có lỗi nghiêm trọng xảy ra'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function getStatisticInYear()
+    {
+        try {
+            $currentYear = now()->year();
+            $startOfLast12Months = now()->subMonths(11)->startOfMonth(); // Start date is last 12 months
+            $endOfMonth = now()->endOfMonth();
+
+            $allMonthsLast12Months = [];
+            $currentMonth = clone $startOfLast12Months;
+            while ($currentMonth <= $endOfMonth) {
+                $allMonthsLast12Months[] = $currentMonth->format('Y-m');
+                $currentMonth->addMonth();
+            }
+
+            $monthlyBookings = Booking::whereBetween('created_at', [$startOfLast12Months, $endOfMonth])
+                ->select(DB::raw('DATE_FORMAT(created_at, "%Y-%m") as month'), DB::raw('count(*) as booking_count'))
+                ->groupBy('month')
+                ->get();
+
+            // Create a new array with default values as 0 for all months in the last 12 months
+            $result = [];
+            $totalRevenue = 0;
+
+            foreach ($allMonthsLast12Months as $month) {
+                $booking = $monthlyBookings->firstWhere('month', $month);
+
+                // Total revenue for a month
+                $revenue = (int) Booking::whereYear('created_at', $currentYear)
+                    ->whereMonth('created_at', Carbon::parse($month)->month)
+                    ->sum('total_price');
+
+                $result[] = [
+                    'month' => $month,
+                    'booking_count' => $booking ? $booking->booking_count : 0,
+                    'revenue' => $revenue
+                ];
+
+                $totalRevenue += $revenue;
+            }
+
+            return response()->json([
+                'data' => [
+                    'monthly_bookings' => $result,
+                    'total_revenue' => $totalRevenue
+                ]
+            ], Response::HTTP_OK);
+        } catch (Exception $exception) {
+            Log::error('StatisticController@getStatisticInMonth: ', [$exception->getMessage()]);
+
+            return response()->json([
+                'message' => 'Đã có lỗi nghiêm trọng xảy ra'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
