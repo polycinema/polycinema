e557be8f556bc441fe91a9aac4265a023c00e681
giang03yn@gmail.com <giang03yn@gmail.com>
2024-01-02 23:36:14 +0700

    forgot password api

---
+++ app/Notifications/ResetPasswordNotification.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ResetPasswordNotification extends Notification
+{
+    use Queueable;
+
+    public $url;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(string $url)
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->line('Quên Mật Khẩu')
+                    ->action('Đặt Lại Mật Khẩu', $this->url)
+                    ->line('Cảm ơn bạn đã sử dụng dịch vụ của chúng tôi!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
