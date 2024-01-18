e2ff028f605e416d6d0291c875df9127159f2e87
giang03yn@gmail.com <giang03yn@gmail.com>
2024-01-18 14:08:08 +0700

    fix mail booking

--- database/migrations/2024_01_16_220235_add_column_to_coupons_table.php
+++ database/migrations/2024_01_16_220235_add_column_to_coupons_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('coupons', function (Blueprint $table) {
-            $table->timestamp('start_at'); // ngày hiệu lực
+            $table->timestamp('start_at')->nullable();
         });
     }
 
