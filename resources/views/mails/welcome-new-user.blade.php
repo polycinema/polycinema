35d70e3ebb73e5d81d63814b4d623fa4a09beaff
hongquando4623 <dohongquan459@gmail.com>
2024-01-18 15:20:04 +0700

    fixmail

--- resources/views/mails/welcome-new-user.blade.php
+++ resources/views/mails/welcome-new-user.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Chào mừng bạn đến với hệ thống</title>
+    <title>Chào mừng   đến với hệ thống</title>
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -62,22 +62,27 @@
     </style>
 </head>
 <body style="font-family: Arial, sans-serif;
-background-color: #f4f4f4;
+
 margin: 0;
 padding: 0;
 text-align: center;
 display: flex;
 align-items: center;
-justify-content: center;
-height: 100vh;">
-    <div class="container" style="background-color: #fff;
+justify-content: center; 
+border-radius: 5px;
+
+text-align: center;
+">
+    <div class="container"style="background-color: #fff;
     border-radius: 8px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    padding: 20px;
-    max-width: 600px;
+    padding: 50px;
+    max-width: 592px;
     width: 100%;
-    text-align: left;">
-        <h2>Chào mừng bạn đến với Polycinema!</h2>
+    text-align: left;
+    border: 1px solid #cdcdcd;
+    margin: 0 auto;
+    ">
+        <h2>Chào mừng {{$user->name}} đến với Polycinema!</h2>
         <p>Cảm ơn bạn đã đăng ký tài khoản. Bây giờ bạn có thể bắt đầu sử dụng dịch vụ của chúng tôi.</p>
         <p>Nếu bạn có bất kỳ câu hỏi nào, vui lòng liên hệ với chúng tôi qua <a href="mailto:upport.polycinema@gmail.com">support.polycinema@gmail.com</a>.</p>
         <p>Xin cảm ơn!</p>
